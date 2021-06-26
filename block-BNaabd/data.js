b4cb3ca7c9a46a1f6545bbae4478dbeb9b7bfcfc
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-06-26 18:40:12 +0530

    refactor

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -4,22 +4,22 @@ var qs = require('querystring');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-  var dataFormat = req.headers['content-type'];
   var store = '';
   req.on('data', (chunk) => {
     store = store + chunk;
   });
   req.on('end', () => {
-    if (dataFormat === 'application/json') {
+    if (req.method === 'POST' && req.url === '/json') {
+      res.setHeader('Content-Type', 'application/json');
       res.end(store);
     }
-    if (dataFormat === 'application/x-www-form-urlencoded') {
+    if (req.method === 'POST' && req.url === '/form') {
       var parsedData = qs.parse(store);
       res.end(JSON.stringify(parsedData));
     }
   });
 }
 
-server.listen(7000, () => {
+server.listen(3000, () => {
   console.log('server listening to the port 7k');
 });
